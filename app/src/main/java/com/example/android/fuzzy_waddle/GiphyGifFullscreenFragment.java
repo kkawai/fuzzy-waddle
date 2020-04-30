219e7123649b7130fefc7ab516a63b2c199c4506
kkawai <kkawai2@gmail.com>
2020-04-30 14:21:45 -0700

    added still image to details and gif progress bar

--- app/src/main/java/com/example/android/fuzzy_waddle/GiphyGifFullscreenFragment.java
+++ app/src/main/java/com/example/android/fuzzy_waddle/GiphyGifFullscreenFragment.java
@@ -3,13 +3,19 @@ package com.example.android.fuzzy_waddle;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.android.fuzzy_waddle.ui.AspectRatioImageView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +25,9 @@ import androidx.fragment.app.Fragment;
 public class GiphyGifFullscreenFragment extends Fragment {
 
   private GiphyGifFullscreenFragment(){}
+
+  private RequestManager requestManager;
+  private AspectRatioImageView aspectRatioImageView;
 
   public static GiphyGifFullscreenFragment newInstance(String gifUrl, String stillUrl, float aspectRatio) {
     Bundle args = new Bundle();
@@ -34,15 +43,41 @@ public class GiphyGifFullscreenFragment extends Fragment {
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.giphy_fullscreen, container, false);
-    RequestBuilder<Drawable> thumbnailRequest = Glide.with(this)
+    requestManager = Glide.with(this);
+    RequestBuilder<Drawable> thumbnailRequest = requestManager
             .load(getArguments().getString("stillUrl")).diskCacheStrategy(DiskCacheStrategy.ALL);
-    AspectRatioImageView aspectRatioImageView = (AspectRatioImageView)view.findViewById(R.id.thumbnail);
+    aspectRatioImageView = view.findViewById(R.id.thumbnail);
     aspectRatioImageView.setAspectRatio(getArguments().getFloat("aspectRatio"));
-    Glide.with(this)
+    requestManager
             .load(getArguments().getString("gifUrl"))
+            .listener(new RequestListener<Drawable>() {
+              @Override
+              public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                //todo
+                return false;
+              }
+
+              @Override
+              public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                try {
+                  getView().findViewById(R.id.progress_horizontal).setVisibility(View.GONE);
+                }catch (Exception e){
+                  //todo
+                }
+                return false;
+              }
+            })
             .thumbnail(thumbnailRequest)
             .into(aspectRatioImageView);
     return view;
   }
 
+  @Override
+  public void onDestroy() {
+    if (requestManager != null && aspectRatioImageView != null) {
+      requestManager.clear(aspectRatioImageView);
+      Log.w("blah","cleared requests");
+    }
+    super.onDestroy();
+  }
 }
